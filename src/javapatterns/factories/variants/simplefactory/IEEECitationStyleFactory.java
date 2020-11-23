5659b0cef075268eda88bc0557df537b9242acc8
Bruno Silva <brunomnsilva@gmail.com>
2020-11-23 11:05:40 +0000

    javadoc.

--- src/javapatterns/factories/variants/simplefactory/IEEECitationStyleFactory.java
+++ src/javapatterns/factories/variants/simplefactory/IEEECitationStyleFactory.java
@@ -19,6 +19,29 @@ import javapatterns.factories.model.IEEEJournalCitation;
  */
 public class IEEECitationStyleFactory {
 
+    /**
+     * Allows the creation of a citation.
+     * <p>
+     * The supported types and required arguments are detailed bellow:
+     * <ul>
+     * <li> Book.
+     *      <b>Type:</b> book
+     *      <b>Args:</b> author, title, place, publisher, year
+     * </li>
+     * <li> Book Chapter.
+     *      <b>Type:</b> bookchapter
+     *      <b>Args:</b> author, title, book_title, editor, place, publisher, page_numbers, year
+     * </li>
+     * <li> Journal.
+     *      <b>Type:</b> journal
+     *      <b>Args:</b> author, title, journal_title, volume_issue, page_numbers, year
+     * </li>
+     * </ul>
+     * </p>
+     * @param type type of publication
+     * @param args list of arguments according to publication type
+     * @return a citation instance.
+     */
     public static Citation create(String type, String... args) {
         switch(type.toLowerCase()) {
             case "book":
